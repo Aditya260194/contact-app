e530d4ebf52ae34f3e426bcfbfb2ea910ea26868
aditya260194 <aditya260194@gmail.com>
2021-07-07 23:55:21 +0530

    use of Link

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ContactCard from './ContactCard';
+import { Link } from 'react-router-dom';
 
 const ContactList = (props) => {
 
@@ -24,6 +25,9 @@ const ContactList = (props) => {
     return (
         <div class='main'>
             <h2>Contact List</h2>
+            <Link to="/add">
+                <button className="ui button blue right">Add Contact</button>
+            </Link>
             <div className='ui celled list'>
                 {renderContactList}
             </div>
