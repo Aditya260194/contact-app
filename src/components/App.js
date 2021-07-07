b10606928e5425f3e8935ab85d75e18145b2a297
aditya260194 <aditya260194@gmail.com>
2021-07-07 23:49:42 +0530

    use of Router, Switch

--- src/components/App.js
+++ src/components/App.js
@@ -5,42 +5,43 @@ import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList.js';
 import { uuid } from 'uuidv4'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
 
- /* const contacts = [
-    {
-      id:1, name: "Aditya", email: "*@gmail.com"
-    },
-    {
-      id:2, name: "Batflix", email: "*@gmail.com"
-    }
-  ]; */
+  /* const contacts = [
+     {
+       id:1, name: "Aditya", email: "*@gmail.com"
+     },
+     {
+       id:2, name: "Batflix", email: "*@gmail.com"
+     }
+   ]; */
   /*will use react hooks to set contacts from add contacts screen 
     This create state for contacts with blank as initial value
   */
   const [contacts, setContacts] = useState([]);
   const LOCAL_STORAGE_KEY = "contacts";
 
-  const addContactHandler = (contact) =>{
-    console.log("addContactHandler " , contact);
+  const addContactHandler = (contact) => {
+    console.log("addContactHandler ", contact);
     /* to add contact to contacts we use setContacts which we we dfined earlier as hook */
     //setContacts([...contacts, contact]);
-    setContacts([...contacts, {id: uuid(), ...contact}]);
+    setContacts([...contacts, { id: uuid(), ...contact }]);
   }
 
   /*storing contacts in local storage*/
   useEffect(() => {
     const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    console.log("use EffectretrieveContacts" , retrieveContacts);
-    if(retrieveContacts) setContacts(retrieveContacts);
-  },[]);
+    console.log("use EffectretrieveContacts", retrieveContacts);
+    if (retrieveContacts) setContacts(retrieveContacts);
+  }, []);
 
-    /*stpring contacts in local storage */
-    useEffect( () => {
-      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
-      console.log("use Effect setItem ");
-    }, [contacts]);
+  /*stpring contacts in local storage */
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+    console.log("use Effect setItem ");
+  }, [contacts]);
 
   const deleteContactHandler = (id) => {
     const newContacts = contacts.filter((contact) => {
@@ -51,11 +52,15 @@ function App() {
 
   return (
     <div className='ui container'>
-      <Header/>
-      {/* to fetch data from child to parent we use handler props*/}
-      <AddContact addContactHandler={addContactHandler}/>
-      {/* passing conatcts as props*/}
-      <ContactList contacts={contacts} getContactId={deleteContactHandler}/>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ContactList} />
+          <Route path="/add" component={AddContact} />
+        </Switch>
+        {/*<AddContact addContactHandler={addContactHandler} /> }
+        {//<ContactList contacts={contacts} getContactId={deleteContactHandler} /> */}
+      </Router>
     </div>
   );
 }
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -8,16 +8,25 @@ const ContactList = (props) => {
         props.getContactId(id);
     };
 
-    const renderContactList = props.contacts.map((contact) => {
+    const contacts = [{
+        id: 1,
+        "name": "A",
+        "email": "a@gmail.com"
+    }];
+
+    const renderContactList = contacts.map((contact) => {
         return (
             <ContactCard contact={contact} clickHandler={getContactId}
-            key={contact.id}></ContactCard>
+                key={contact.id}></ContactCard>
         );
     }
     );
     return (
-        <div className='ui celled list'>
-            {renderContactList}
+        <div class='main'>
+            <h2>Contact List</h2>
+            <div className='ui celled list'>
+                {renderContactList}
+            </div>
         </div>
     );
 }
